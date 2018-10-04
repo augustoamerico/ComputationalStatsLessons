35121f2822036bf0422402ae6ba9f1f7ee489d40
tiago.correia <tiago.correia@feedzai.com>
2018-10-04 16:20:56 +0100

    docs folder

--- _book/mathjax.js
+++
@@ -1,7 +0,0 @@
-(function () {
-console.log("------||")
-var script = document.createElement("script");
-script.type = "text/javascript";
-script.src  = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
-document.getElementsByTagName("head")[0].appendChild(script);
-})();
